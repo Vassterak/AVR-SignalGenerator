c4fc02be17ed61d69635053e9e66174432394c0a
Vassterak <35633483+Vassterak@users.noreply.github.com>
2022-01-29 19:44:33 +0100

    sine generator + libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,74 @@
 /*
 Arduino library not used because it cannot be used in my homework,
-also arduino's int is 16bit but AVR's is 8 bit.
+also arduino's uint8_t is 16bit but AVR's is 8 bit.
 */ 
-//#include <Arduino.h>
-#include <avr/io.h>
-#include <util/delay.h> 
+#include <Arduino.h> //only used for Serial communication
+ #include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+extern "C" { 
 #include "usart.h"
+}
 
-int channel1 = 0, channel2 = 0;
+uint8_t channel1Singal[256], channel2Signal[256];
 
 void setup()
 {
+  Serial.begin(9600);
+  //usart_setup();
+
   DDRA = 0xff; //set whole Port A as output CHANNEL 1 8bits
   DDRC = 0xff; //set whole Port C as output CHANNEL 2 8bits
 }
 
-void OutputUpdate(int channel1)
+void createSinWave(uint8_t channelID)
+{
+  for (int16_t i = 0; i <= 0xff; i++)
+  {
+    if (channelID == 1)
+        channel1Singal[i] = 0x7f + (0x7f * sin(2* M_PI / 0xff * (i+1)));
+
+    else
+        channel2Signal[i] = 0x7f + (0x7f * sin(2* M_PI / 0xff * (i+1)));
+  }
+}
+
+void OutputUpdate(uint8_t channel1)
 {
   PORTA = channel1;
 }
 
-void OutputUpdate(int channel1, int channel2)
+void OutputUpdate(uint8_t channel1, uint8_t channel2)
 {
   PORTA = channel1;
   PORTC = channel2;
 }
 
+/* void demo_chargen(void) {
+  static uint8_t c=32;
+  for (c=32;c<128;c++) {
+    usart_putchar(c);
+  }
+  usart_putchar(10);
+  usart_putchar(13);
+} */
+
+
 int main(void)
 {
   setup();
-
+  uint8_t a = 0;
   while (1)
   {
-    _delay_ms(1);
+    PORTA = channel1Singal[a];
+    a++;
+    _delay_ms(5);
+    //demo_chargen();
     //channel1 = channel1 << 1;
-    channel1++;;
-    OutputUpdate(channel1);
+    //channel1++;
+    //OutputUpdate(channel1);
+    //PORTA++;
   }
-  
   return 0;
 }
+
